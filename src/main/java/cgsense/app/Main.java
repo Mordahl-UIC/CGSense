476283682d69a53e6b7be00789cf97e4197f0d6b
0men1 <homahomen2@gmail.com>
2026-09-15 05:17:45 +0000

    added a raw file fetcher

--- src/main/java/cgsense/app/Main.java
+++ src/main/java/cgsense/app/Main.java
@@ -1,30 +1,58 @@
 package cgsense.app;
 
+import org.kohsuke.github.GHCommit;
 import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
 import cgsense.astdiff.GumTreeRunner;
 import cgsense.discovery.GithubClientFactory;
-import cgsense.discovery.RepositoryDiscovery;
-import cgsense.reader.ResourceReader;
+import cgsense.discovery.RawFetcher;
 
 public class Main {
 
   public static void main(String[] args) {
     try {
       GumTreeRunner runner = new GumTreeRunner();
-      String before = ResourceReader.read("test_files/TestBefore1.java");
-      String after = ResourceReader.read("test_files/TestAfter1.java");
 
-      var r = runner.diff(before, after);
-      r.script().forEach(a -> System.out.printf("%s\n\n", a.toString()));
+      GitHub client = GithubClientFactory.create();
 
-      /*
-       * RepositoryDiscovery discovery = new
-       * RepositoryDiscovery(GithubClientFactory.create());
-       * 
-       * for (GHRepository repo : discovery.topJava(10)) {
-       * System.out.printf("%s\n", repo.getFullName());
-       * }
-       */
+      GHRepository repo = client.getRepository("jhy/jsoup");
+      RawFetcher raw = new RawFetcher("jhy", "jsoup");
+      System.out.printf("%s\n", repo.getDescription());
+
+      for (GHCommit commit : repo.listCommits()) {
+        if (commit.getParents().size() > 1)
+          continue;
+
+        String afterSha = commit.getSHA1();
+        String beforeSha = commit.getParents().get(0).getSHA1();
+
+        for (GHCommit.File file : commit.listFiles()) {
+          if (file.getFileName().contains(".java")) {
+            if (file.getLinesAdded() < 10) {
+
+              String pathAfter = file.getFileName();
+              String pathBefore = file.getPreviousFilename() != null
+                  ? file.getPreviousFilename()
+                  : file.getFileName();
+
+              String before, after;
+
+              try {
+                before = raw.get(beforeSha, pathBefore);
+                after = raw.get(afterSha, pathAfter);
+
+                var r = runner.diff(before, after);
+                r.script().forEach(a -> System.out.printf("%s\n\n", a.toString()));
+                return;
+
+              } catch (Exception e) {
+                e.printStackTrace();
+              }
+            }
+          }
+        }
+      }
 
     } catch (Exception e) {
       e.printStackTrace();
