b32b2f6125d84f5d72bbee018f8e8c5cb95ee5f0
0men1 <homahomen2@gmail.com>
2026-09-25 03:17:29 +0000

    custom criteria parsing

--- src/main/java/cgsense/app/cmd/DiscoverCommand.java
+++ src/main/java/cgsense/app/cmd/DiscoverCommand.java
@@ -1,5 +1,8 @@
 package cgsense.app.cmd;
 
+import java.io.File;
+import java.io.InputStream;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
@@ -20,24 +23,39 @@ public class DiscoverCommand implements Command {
         .addOption(Option.builder()
             .longOpt("limit")
             .hasArg().type(Number.class)
+            .build())
+        .addOption(Option.builder()
+            .longOpt("criteria")
+            .hasArg()
+            .type(File.class)
             .build());
   }
 
   @Override
   public int run(CommandLine cl) throws Exception {
-    int limit = Integer.parseInt(cl.getOptionValue("limit"));
-
-    String fakeJson = """
-        {
-          "minStars": 10,
-          "buildSystems": ["maven", "gradle"]
-        }
-        """;
     ObjectMapper mapper = new ObjectMapper();
-    Criteria criteria = mapper.readValue(fakeJson, Criteria.class);
+
+    int limit = Integer.parseInt(cl.getOptionValue("limit"));
+    Criteria criteria = null;
+
+    if (cl.hasOption("criteria")) {
+      criteria = mapper.readValue(cl.getOptionValue("criteria"), Criteria.class);
+    } else {
+      try (InputStream is = DiscoverCommand.class.getClassLoader().getResourceAsStream("default-repo-criteria.json")) {
+        criteria = mapper.readValue(is, Criteria.class);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    if (criteria == null) {
+      System.err.println("Could not load criteria");
+      return 1;
+    }
 
     System.out.printf("minStars: %d\n", criteria.minStars());
-    for (String buildSystem : criteria.buildSystems()) {
+    for (
+
+    String buildSystem : criteria.buildSystems()) {
       System.out.printf("buildSystem: %s\n", buildSystem);
     }
 
